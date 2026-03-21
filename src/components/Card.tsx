d1722a9c7cf337387c5a39f9bf229d3ac21bb242
espanamanu41-eng <espanamanu41@gmail.com>
2026-03-20 23:26:22 -0700

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,1 +1,20 @@
+type Props={
+title?:string
+children:React.ReactNode
+}
 
+export default function Card({title,children}:Props){
+
+return(
+
+<section className="card">
+
+{title && <h3>{title}</h3>}
+
+{children}
+
+</section>
+
+)
+
+}
