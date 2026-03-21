0b4c54d5ed2a0f7a007aeb535b50d339e6624b02
espanamanu41-eng <espanamanu41@gmail.com>
2026-03-20 18:21:15 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ export default function App() {
   return (
     <div>
       <h1>App funcionando 🚀</h1>
-      <p>Ahora sí debe servir</p>
-
       <TrendingUp size={20} />
       <Wallet size={20} />
       <Home size={20} />
